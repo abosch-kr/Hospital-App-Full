90f33e3e504579262796f8c009f974cb300b24f6
abosch-kr <anthony.bosch@kroger.com>
2021-12-02 14:18:09 -0500

    Added rest api's

--- src/main/java/com/example/hospitalapp/controller/DoctorController.java
+++ src/main/java/com/example/hospitalapp/controller/DoctorController.java
@@ -2,12 +2,53 @@ package com.example.hospitalapp.controller;
 
 
 import com.example.hospitalapp.model.Doctor;
+import com.example.hospitalapp.repository.DoctorRepository;
 import com.example.hospitalapp.view.CLIView;
 import com.example.hospitalapp.view.View;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctor/api")
 public class DoctorController extends BaseController {
+    @Autowired
+    private DoctorRepository repository;
+
+    @GetMapping("/")
+    public List<Doctor> getDoctors() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Doctor getDoctor(@PathVariable String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/")
+    public Doctor postDoctor(@RequestBody Doctor doctor) {
+        return repository.save(doctor);
+    }
+
+    @PutMapping("/")
+    public Doctor putDoctor(@RequestBody Doctor updatedDoctor) {
+        Doctor doctor = repository.findById(updatedDoctor.getId()).orElse(null);
+        assert doctor != null;
+        doctor.setName(updatedDoctor.getName());
+        return doctor;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteDoctor(@PathVariable String id) {
+        repository.deleteById(id);
+        return id;
+    }
+
     private final View   view;
     private       Doctor doctor;
+
+
 
     public DoctorController()
     {

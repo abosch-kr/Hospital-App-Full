e8455cd5fd825d09d24e81cf582e7413b247ee18
abosch-kr <anthony.bosch@kroger.com>
2021-12-02 13:30:44 -0500

    Added id field

--- src/main/java/com/example/hospitalapp/model/Doctor.java
+++ src/main/java/com/example/hospitalapp/model/Doctor.java
@@ -1,7 +1,12 @@
 package com.example.hospitalapp.model;
 
+import org.springframework.data.annotation.Id;
+
 public class Doctor
 {
+    @Id
+    private String id;
+
     private String name;
 
     public Doctor(String name)
@@ -17,5 +22,10 @@ public class Doctor
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    public String getId()
+    {
+        return id;
     }
 }

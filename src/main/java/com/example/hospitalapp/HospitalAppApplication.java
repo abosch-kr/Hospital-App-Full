2692ad35e5f524af82a5de10146af1bcea04be08
abosch-kr <anthony.bosch@kroger.com>
2021-12-02 14:18:24 -0500

    Added swagger configuration

--- src/main/java/com/example/hospitalapp/HospitalAppApplication.java
+++ src/main/java/com/example/hospitalapp/HospitalAppApplication.java
@@ -1,25 +1,14 @@
 package com.example.hospitalapp;
 
-import com.example.hospitalapp.controller.Controller;
-import com.example.hospitalapp.view.CLIView;
-import com.example.hospitalapp.view.View;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class HospitalAppApplication implements CommandLineRunner
+@EnableSwagger2
+public class HospitalAppApplication
 {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalAppApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception
-	{
-		View view = new CLIView();
-		Controller controller = new Controller(view);
-		controller.simulateOperation();
 	}
 }

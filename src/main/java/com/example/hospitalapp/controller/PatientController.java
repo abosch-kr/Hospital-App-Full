0c62fb18d4f3f18ffb532489edd43cae62b8246e
abosch-kr <anthony.bosch@kroger.com>
2021-12-02 15:01:29 -0500

    First commit

---
+++ src/main/java/com/example/hospitalapp/controller/PatientController.java
@@ -0,0 +1,5 @@
+package com.example.hospitalapp.controller;
+
+public class PatientController
+{
+}
---
+++ src/main/java/com/example/hospitalapp/repository/PatientRepository.java
@@ -0,0 +1,5 @@
+package com.example.hospitalapp.repository;
+
+public interface PatientRepository
+{
+}

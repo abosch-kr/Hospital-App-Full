0609636950b9a57d958fbb93c3bfc5c81002b689
abosch-kr <anthony.bosch@kroger.com>
2021-12-28 16:11:57 -0500

    added specialty attribute and split into first name last name

--- src/main/java/com/example/hospitalapp/model/Doctor.java
+++ src/main/java/com/example/hospitalapp/model/Doctor.java
@@ -1,38 +1,58 @@
 package com.example.hospitalapp.model;
 
-import com.example.hospitalapp.repository.DoctorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Doctor implements HospitalEntity
 {
     @Id
     private String id;
+    private String firstName;
+    private String lastName;
+    private String specialty;
 
-    @Autowired
-    private DoctorRepository repository;
-
-    private String name;
-
-    public Doctor(String name)
+    public Doctor(String firstName, String lastName, String specialty)
     {
-        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.specialty = specialty;
     }
 
     public Doctor() { }
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
     public String getId()
     {
         return id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getSpecialty()
+    {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty)
+    {
+        this.specialty = specialty;
     }
 }

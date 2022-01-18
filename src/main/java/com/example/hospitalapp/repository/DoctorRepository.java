ea539c31053eec8734c6824094692a3de815a771
Anthony Bosch <anthony.bosch@kroger.com>
2022-01-18 15:00:21 -0500

    first commit

--- src/main/java/com/example/hospitalapp/repository/DoctorRepository.java
+++ src/main/java/com/example/hospitalapp/repository/DoctorRepository.java
@@ -1,8 +1,10 @@
 package com.example.hospitalapp.repository;
 
 import com.example.hospitalapp.model.Doctor;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DoctorRepository extends MongoRepository<Doctor, String> {}
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+}

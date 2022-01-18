40f61d878db1bb07b187264a08743b39e02dc01a
Anthony Bosch <anthony.bosch@kroger.com>
2022-01-18 14:59:19 -0500

    commented out for now

--- src/main/java/com/example/hospitalapp/controller/BaseController.java
+++ src/main/java/com/example/hospitalapp/controller/BaseController.java
@@ -1,7 +1,5 @@
 package com.example.hospitalapp.controller;
 
-import com.example.hospitalapp.model.DataEntity;
-import com.example.hospitalapp.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,31 +9,31 @@ import java.util.*;
 @RequestMapping("/api/")
 public class BaseController {
 
-    @Autowired
-    private Map<String, DataService> services;
-
-    @PostMapping("{entityType}")
-    public DataEntity post(@RequestBody DataEntity entity, @PathVariable String entityType) {
-        return services.get(entityType).create(entity);
-    }
-
-    @GetMapping("{entityType}")
-    public List<? extends DataEntity> get(@PathVariable String entityType) {
-        return services.get(entityType).readAll();
-    }
-
-    @GetMapping("{entityType}/{id}")
-    public Optional<? extends DataEntity> get(@PathVariable String entityType, @PathVariable String id) {
-        return services.get(entityType).read(id);
-    }
-
-    @PutMapping("{entityType}/{id}")
-    public DataEntity update(@PathVariable String entityType, @PathVariable String id, @RequestBody DataEntity entity) {
-        return services.get(entityType).update(id, entity);
-    }
-
-    @DeleteMapping("{entityType}/{id}")
-    public String delete(@PathVariable String entityType, @PathVariable String id) {
-        return services.get(entityType).delete(id);
-    }
+//    @Autowired
+//    private Map<String, DataService> services;
+//
+//    @PostMapping("{entityType}")
+//    public DataEntity post(@RequestBody DataEntity entity, @PathVariable String entityType) {
+//        return services.get(entityType).create(entity);
+//    }
+//
+//    @GetMapping("{entityType}")
+//    public List<? extends DataEntity> get(@PathVariable String entityType) {
+//        return services.get(entityType).readAll();
+//    }
+//
+//    @GetMapping("{entityType}/{id}")
+//    public Optional<? extends DataEntity> get(@PathVariable String entityType, @PathVariable String id) {
+//        return services.get(entityType).read(id);
+//    }
+//
+//    @PutMapping("{entityType}/{id}")
+//    public DataEntity update(@PathVariable String entityType, @PathVariable String id, @RequestBody DataEntity entity) {
+//        return services.get(entityType).update(id, entity);
+//    }
+//
+//    @DeleteMapping("{entityType}/{id}")
+//    public String delete(@PathVariable String entityType, @PathVariable String id) {
+//        return services.get(entityType).delete(id);
+//    }
 }

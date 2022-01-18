0b635536bd1d473478add4012206f39477c2aeb7
Anthony Bosch <anthony.bosch@kroger.com>
2022-01-18 14:59:57 -0500

    structured to use jpa and lombok annotations

--- src/main/java/com/example/hospitalapp/model/Doctor.java
+++ src/main/java/com/example/hospitalapp/model/Doctor.java
@@ -1,107 +1,33 @@
 package com.example.hospitalapp.model;
 
-import com.example.hospitalapp.repository.DoctorRepository;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.Id;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
+import javax.persistence.*;
 
-@Component("doctor")
-@JsonTypeName(value = "doctor")
-public class Doctor implements DataEntity
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Table(name = "tbl_doctor")
+public class Doctor
 {
     @Id
-    private String id;
+    @SequenceGenerator(
+            name = "doctor_sequence",
+            sequenceName = "doctor_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "doctor_sequence"
+    )
+    private Long doctorId;
     private String firstName;
     private String lastName;
     private String specialty;
-
-    @Autowired
-    private DoctorRepository repository;
-
-    public Doctor(String firstName, String lastName, String specialty)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.specialty = specialty;
-    }
-
-    public Doctor() {
-
-    }
-
-    public String getId()
-    {
-        return id;
-    }
-
-    public String getFirstName()
-    {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName)
-    {
-        this.firstName = firstName;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
-
-    public void setLastName(String lastName)
-    {
-        this.lastName = lastName;
-    }
-
-    public String getSpecialty()
-    {
-        return specialty;
-    }
-
-    public void setSpecialty(String specialty)
-    {
-        this.specialty = specialty;
-    }
-
-//    @Override
-    public Doctor create(DataEntity doctor)
-    {
-        return repository.save((Doctor) doctor);
-    }
-//
-//    @Override
-//    public Doctor update(String id, DataEntity updatedDoctor)
-//    {
-//        Doctor doctor = repository.findById(id).orElse(null);
-//        assert doctor != null;
-//        doctor.setFirstName(((Doctor)updatedDoctor).getFirstName());
-//        doctor.setLastName(((Doctor)updatedDoctor).getLastName());
-//        doctor.setSpecialty(((Doctor)updatedDoctor).getSpecialty());
-//
-//        return repository.save(doctor);
-//    }
-//
-//    @Override
-//    public List<Doctor> readAll()
-//    {
-//        return repository.findAll();
-//    }
-//
-//    @Override
-//    public Optional<Doctor> read(String id)
-//    {
-//        return repository.findById(id);
-//    }
-//
-//    @Override
-//    public String delete(String id)
-//    {
-//        repository.deleteById(id);
-//        return id;
-//    }
 }
+
--- src/main/java/com/example/hospitalapp/model/Patient.java
+++ src/main/java/com/example/hospitalapp/model/Patient.java
@@ -1,174 +1,36 @@
 package com.example.hospitalapp.model;
 
-import com.example.hospitalapp.repository.PatientRepository;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.Id;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.*;
 import java.util.List;
-import java.util.Optional;
 
 
-@Component("patient")
-@JsonTypeName("patient")
-public class Patient implements DataEntity
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Table(name = "tbl_patient")
+public class Patient
 {
-    @Id private String id;
+    @Id
+    private String id;
     private String firstName;
     private String middleName;
     private String lastName;
     private String suffix;
+    @ElementCollection
     private List<String> ailments;
-    private int priority;
+    @OneToOne
+    @JoinColumn(
+            name = "doctor_id",
+            referencedColumnName = "doctorId"
+    )
     private Doctor doctor;
     private boolean treated;
 
-    public Patient()
-    {
-    }
-
-    public Patient(String firstName, String middleName, String lastName, String suffix,
-            List<String> ailments, int priority, Doctor doctor)
-    {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.suffix = suffix;
-        this.ailments = ailments;
-        this.priority = priority;
-        this.doctor = doctor;
-        this.treated = false;
-    }
-
-    @Id
-    public String getId()
-    {
-        return id;
-    }
-
-//
-//    @Autowired
-//    private PatientRepository repository;
-
-    public String getFirstName()
-    {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName)
-    {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName()
-    {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName)
-    {
-        this.middleName = middleName;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
-
-    public void setLastName(String lastName)
-    {
-        this.lastName = lastName;
-    }
-
-    public String getSuffix()
-    {
-        return suffix;
-    }
-
-    public void setSuffix(String suffix)
-    {
-        this.suffix = suffix;
-    }
-
-    public List<String> getAilments()
-    {
-        return ailments;
-    }
-
-    public void setAilments(List<String> ailments)
-    {
-        this.ailments = ailments;
-    }
-
-    public int getPriority()
-    {
-        return priority;
-    }
-
-    public void setPriority(int priority)
-    {
-        this.priority = priority;
-    }
-
-    public Doctor getDoctor()
-    {
-        return doctor;
-    }
-
-    public void setDoctor(Doctor doctor)
-    {
-        this.doctor = doctor;
-    }
-
-    public boolean isTreated()
-    {
-        return treated;
-    }
-
-    public void setTreated(boolean treated)
-    {
-        this.treated = treated;
-    }
-
-//    @Override
-//    public Patient create(DataEntity patient)
-//    {
-//        return repository.save((Patient) patient);
-//    }
-//
-//    @Override
-//    public Patient update(String id, DataEntity updatedPatient)
-//    {
-//        Patient patient = repository.findById(id).orElse(null);
-//        assert patient != null;
-//        patient.setDoctor(((Patient)updatedPatient).getDoctor());
-//        patient.setAilments(((Patient)updatedPatient).getAilments());
-//        patient.setFirstName(((Patient)updatedPatient).getFirstName());
-//        patient.setLastName(((Patient)updatedPatient).getLastName());
-//        patient.setSuffix(((Patient)updatedPatient).getSuffix());
-//        patient.setMiddleName(((Patient)updatedPatient).getMiddleName());
-//        patient.setPriority(((Patient)updatedPatient).getPriority());
-//
-//        return repository.save(patient);
-//    }
-//
-//    @Override
-//    public List<Patient> readAll()
-//    {
-//        return repository.findAll();
-//    }
-//
-//    @Override
-//    public Optional<Patient> read(String id)
-//    {
-//        return repository.findById(id);
-//    }
-//
-//    @Override
-//    public String delete(String id)
-//    {
-//        repository.deleteById(id);
-//        return id;
-//    }
 }

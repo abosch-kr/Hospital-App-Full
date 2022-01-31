5738b79ba1fccf00a28dd393cdf726aa08a7d591
Anthony Bosch <anthony.bosch@kroger.com>
2022-01-31 15:33:20 -0500

    first commit

---
+++ src/main/java/com/example/hospitalapp/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.example.hospitalapp.repository;
+
+import com.example.hospitalapp.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
---
+++ src/test/java/com/example/hospitalapp/repository/PatientRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.hospitalapp.repository;
+
+import com.example.hospitalapp.model.Doctor;
+import com.example.hospitalapp.model.Patient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PatientRepositoryTest {
+
+    @Autowired PatientRepository repository;
+
+    @Test
+    public void savePatient() {
+        Doctor doctor = Doctor.builder()
+                .firstName("tom")
+                .lastName("smith")
+                .specialty("dentist")
+                .build();
+
+        Patient patient = Patient.builder()
+                .firstName("anthony")
+                .middleName("jose")
+                .lastName("bosch")
+                .suffix("jr")
+                .ailments(List.of("headache", "stomachache", "rash"))
+                .treated(false)
+                .doctor(doctor)
+                .build();
+
+        repository.save(patient);
+    }
+}
